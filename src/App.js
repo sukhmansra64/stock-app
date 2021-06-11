e3e8eed8ffbbcc2559374b59a840e0d14801b7a8
sukhmansra64 <76924528+sukhmansra64@users.noreply.github.com>
2021-06-11 19:15:27 -0400

    Working with the api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import 'strict';
 import logo from './logo.svg';
 import './App.css';
+const request = require('request');
+let stream;
+let partialMessage;
+const connect = ()=>{
+  stream = request({
+    url: 'https://cloud-sse.iexapis.com/stable/stocksUSNoUTP?token=pk_5600d65a8fcc45418dcb19281fc77e47&symbols=spy,ibm,twtr',
+    headers:{
+      'Content-Type': 'text/event-stream'
+    }
+  })
+}
+connect();
+stream.on('socket',()=>{
+  console.log("Connected");
+});
+stream.on('end',()=>{
+  console.log("Reconnecting");
+  connect();
+});
+stream.on('complete',()=>{
+  console.log("Reconnecting");
+  connect();
+});
+stream.on('error',(err)=>{
+  console.log("Error",err);
+  connect();
+});
+//go to documentation of the api and complete the code above
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React>
     <App />
-  </React.StrictMode>,
+  </React>,
   document.getElementById('root')
 );
 
