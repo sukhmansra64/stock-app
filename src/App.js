6b3f56bb7820596720846334c9e6075154d3e475
sukhmansra64 <76924528+sukhmansra64@users.noreply.github.com>
2021-06-12 17:42:30 -0400

    Update
    Implemented React-Bootstrap along with coming up with the basic formatting of the app. Also began to work on usage of the stock API to fetch data and learning the implementation.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,39 @@
-import 'strict';
-import logo from './logo.svg';
+
 import './App.css';
-const request = require('request');
-let stream;
-let partialMessage;
-const connect = ()=>{
-  stream = request({
-    url: 'https://cloud-sse.iexapis.com/stable/stocksUSNoUTP?token=pk_5600d65a8fcc45418dcb19281fc77e47&symbols=spy,ibm,twtr',
-    headers:{
-      'Content-Type': 'text/event-stream'
-    }
-  })
+import 'bootstrap/dist/css/bootstrap.min.css';
+let stock="aapl";
+const url=`https://cloud.iexapis.com/stable/stock/${stock}/quote?token=pk_5600d65a8fcc45418dcb19281fc77e47`
+
+const fetchRequest = async (url)=>{
+  try{
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+  } catch(err){
+    console.log(err)
+    return err;
+  }
 }
-connect();
-stream.on('socket',()=>{
-  console.log("Connected");
-});
-stream.on('end',()=>{
-  console.log("Reconnecting");
-  connect();
-});
-stream.on('complete',()=>{
-  console.log("Reconnecting");
-  connect();
-});
-stream.on('error',(err)=>{
-  console.log("Error",err);
-  connect();
-});
-//go to documentation of the api and complete the code above
 
 function App() {
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={'app'}>
+        <div className={'container'}>
+          <h1>Sukhman Sra's Stock App</h1>
+        </div>
+        <div className={'container'}>
+          <table className={'table mt-5'}>
+            <thead>
+              <tr>
+                <th>Ticker</th>
+                <th>Price</th>
+                <th>Date</th>
+                <th>Time</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
   );
 }
 
