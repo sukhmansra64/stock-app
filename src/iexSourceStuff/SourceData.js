b68c1faeef4c934e3bda925bea5682ecfee5b0c1
sukhmansra64 <76924528+sukhmansra64@users.noreply.github.com>
2021-07-11 19:39:23 -0400

    Update SourceData.js

--- src/iexSourceStuff/SourceData.js
+++ src/iexSourceStuff/SourceData.js
@@ -1,4 +1,5 @@
 //the API token to be used to provide search results, and stock information
+//add an iex cloud premium API key here
 export const iexData={
-    apiToken: 'pk_5600d65a8fcc45418dcb19281fc77e47 ',
+    apiToken: 'placeholder',
 }
